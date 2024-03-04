4e740a0612a0d1d040b769ea31f9deccf4aac461
Mark Kelsall <markkelsall1995@gmail.com>
2024-03-04 09:17:56 +0300

    final

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,8 +1,19 @@
 class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
+        self._size = None
         self.size = size
         self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        self._size = value
 
     def cobble(self):
         self.condition = "New"
